d3d7183f9d28e7ec5306c2b5704936528f7c0de3
thwishag <42414421+thwishag@users.noreply.github.com>
2018-10-29 20:26:23 -0700

    Update DeckOfCardsTest.java
    Almost done

--- src/CardGame/DeckOfCardsTest.java
+++ src/CardGame/DeckOfCardsTest.java
@@ -1,72 +1,77 @@
 package CardGame;
 import java.util.ArrayList;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 /**
  * Name: Thwisha Guru
  * Date: 10/25/18
  * Period: 1
  *
- * CardGame.DeckOfCardsTest.java
+ * CardGame.BlackJack.java
  *
  * Tester program to shuffle and deal a deck of Card objects
  */
 
-public class DeckOfCardsTest
-{
-    public static void main (String[] args)
-    {
+public class BlackJack {
+    public static void main(String[] args) {
+        System.out.println("Hello and let us begin!");
+
         DeckOfCards myDeckOfCards = new DeckOfCards();
-        Scanner scan = new Scanner(System.in);
-        myDeckOfCards.shuffle(); // put Card objects in random order
-        ArrayList<Card> player = new ArrayList<Card>();
-        ArrayList<Card> dealer = new ArrayList<Card>();
-        int playerValue = 0, dealerValue = 0;
-        boolean isItBust = false;
+        myDeckOfCards.generate();
+        myDeckOfCards.shuffle();
 
-        //First 2 cards
-        for(int i =0; i < 2; i++)
-        {
-            dealer.add( myDeckOfCards.dealCard());
-            player.add( myDeckOfCards.dealCard());
+        DeckOfCards player = new DeckOfCards();
+        DeckOfCards dealer = new DeckOfCards();
+
+        Scanner userInput = new Scanner(System.in);
+        boolean end = false;
+        //Player gets 2 cards
+        player.draw(myDeckOfCards);
+        player.draw(myDeckOfCards);
+        //Dealer gets 2 cards
+        dealer.draw(myDeckOfCards);
+        dealer.draw(myDeckOfCards);
+
+        while (true) {
+            System.out.println("Your hand is: ");
+            System.out.print(player.toString());
+            System.out.println("Your deck has a value of: " + player.valueofcard());
+
+            System.out.println("Dealer Hand is: " + dealer.getCard(0).toString());
+
+            System.out.println("Would you like to Hit or Stay?");
+            int answer = userInput.nextInt();
+
+            //Hit
+            if (answer == 1) {
+                player.draw(myDeckOfCards);
+                System.out.println("You drew a: " + player.getCard(player.deckSize() - 1).toString());
+                //Bust if they end up going over 21
+                if (player.valueofcard() > 21) {
+                    System.out.println("you BUSTED! and your current value is:" + player.valueofcard());
+                    end = true;
+                    break;
+                }
+                if (answer == 2) {
+                    break;
+                }
+            }
+            //The dealer's cards are now revealed.
+            System.out.println("The Dealer's Cards: " + dealer.toString());
+            if (dealer.valueofcard() > player.valueofcard() && end == false) {
+                System.out.println("The Dealer won!");
+                throw new InputMismatchException();
+            }
+            while ((dealer.valueofcard() < 17) && end == false) {
+                dealer.draw(myDeckOfCards);
+                System.out.println("Dealer draws: " + dealer.getCard(dealer.deckSize() - 1).toString());
+            }
+            System.out.println("The dealer's hand value is: " + dealer.valueofcard());
+            if ((dealer.valueofcard() > 21) && end == false) {
+                System.out.println("The Dealer busted! You win!!");
+                end = true;
+            }
         }
-        //Option-2 Solution to the assignment - 0-1
-        System.out.println("The Dealers First Card: " + dealer.get(0).toString());
-        //if (dealer)
-        System.out.printf("The Players First Card: %s, Second Card: %s\n", player.get(0).toString(), player.get(1).toString());
-        playerValue = player.get(0).getvalue() + player.get(1).getvalue();
-        dealerValue = dealer.get(0).getvalue() + dealer.get(1).getvalue();
-        System.out.println("The Total Value of the Cards is " + playerValue + ".");
-
-        System.out.print("Would you like to hit or stay?");
-        String response = scan.nextLine();
-
-
-//        if ((playerValue == 21)) {
-//            System.out.println("Player has won");
-//        } else {
-//
-//        }
-//        do {
-//
-//
-//        }//
-
-
-//        // -----------------------------------------------------------
-//        //  print all 52 Cards in the order in which they are dealt
-//        // -----------------------------------------------------------
-//        for (int i = 0; i < 13; i++)
-//        {
-//            // printf method is used for formatting output
-//            // print string (%) in a space of 20 characters (20s)
-//            // the - in the (-20s) makes the String left align
-//            // printf can be used with floats, ints and other types as well
-//            // cheat sheet @ https://thejavacheatsheet.wordpress.com/2015/04/11/printf/
-//            // Guide #2 https://www.cs.colostate.edu/~cs160/.Summer16/resources/Java_printf_method_quick_reference.pdf
-//            System.out.printf("%-20s%-20s%-20s%-20s\n",
-//                myDeckOfCards.dealCard(), myDeckOfCards.dealCard(),
-//                myDeckOfCards.dealCard(), myDeckOfCards.dealCard());
-//
-//        }
     }
 }
+
